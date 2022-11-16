bfa4238e22b04054cdf619c1915a04c045420d69
armandlepri <lepriarmand@gmail.com>
2022-11-16 22:45:06 +0000

    creation de Header

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import SignUp from "./Pages/SignUp";
 import Profile from "./Pages/Profile";
 import Offers from "./Pages/Offers";
 import ForgotPassword from "./Pages/ForgotPassword";
+import Header from "./components/Header";
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
+				<Header />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/sign-in" element={<SignIn />} />
